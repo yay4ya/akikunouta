ea27babbda3067c07a5713c89139cd1c6f42366f
yay4ya <yay4ya.dev@gmail.com>
2020-12-28 19:57:09 +0900

    add validations

--- scripts/validate_data.py
+++ scripts/validate_data.py
@@ -2,7 +2,7 @@ import dataclasses
 import json
 import logging
 import sys
-from collections import Counter
+from collections import defaultdict, Counter
 from typing import Callable, Dict, List
 
 logger = logging.getLogger('data validator')
@@ -83,6 +83,42 @@ def validate_ids(tracks: List[Track]) -> ValidationResult:
             is_valid=False,
             message=f"Duplicated IDs: {duplicated_ids}",
         )
+    return ValidationResult(True, "OK")
+
+
+@validator.add("validate published date")
+def validate_published_at(tracks: List[Track]) -> ValidationResult:
+    videoToTracks: Dict[str, List[Track]] = defaultdict(list)
+    for track in tracks:
+        videoToTracks[track.videoid].append(track)
+
+    invalid_videoids: List[str] = []
+    for videoid, target_tracks in videoToTracks.items():
+        dates = [track.publishedAt for track in target_tracks]
+
+        if len(set(dates)) > 1:
+            invalid_videoids.append(videoid)
+
+    if invalid_videoids:
+        return ValidationResult(
+            is_valid=False,
+            message=f"Published date not matched: {invalid_videoids}")
+
+    return ValidationResult(True, "OK")
+
+
+@validator.add("validate start / end time")
+def validate_start_end_time(tracks: List[Track]) -> ValidationResult:
+    invalid_ids: List[int] = []
+    for track in tracks:
+        if track.start > track.end:
+            invalid_ids.append(track.id)
+
+    if invalid_ids:
+        return ValidationResult(
+            is_valid=False,
+            message=f"Start time must be less than end time: {invalid_ids}")
+
     return ValidationResult(True, "OK")
 
 
